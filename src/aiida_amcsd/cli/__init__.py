487a1a827491048e9cb168567707cbdcfedaa612
Sebastiaan Huber <mail@sphuber.net>
2022-03-10 14:52:34 +0100

    CLI: add the `scrape` command to scrape CIFs and store in AiiDA
    This command will scrape the contents of the AMCSD mineral index to get
    a list of available minerals. For each mineral, its dedicated page will
    then be scraped for anchors that allow downloading its structure in CIF
    format. These are then downloaded and wrapped in `SinglefileData` nodes
    that are added to a `Group`. The source metadata and md5 of the
    retrieved CIF files are added as attributes to the `SinglefileData`.
    
    The `requests-cache` library is used to cache the requests such that the
    server is not overloaded on subsequent invocations of the command.

--- src/aiida_amcsd/cli/__init__.py
+++ src/aiida_amcsd/cli/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 """Command line interface for ``aiida-amcsd``."""
 from .cli import cli
+from .cmd_scrape import cmd_scrape
---
+++ src/aiida_amcsd/cli/cmd_scrape.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""CLI command to scrape the AMCSD to download structures in CIF format."""
+from aiida.cmdline.params.types import GroupParamType
+import click
+
+from .cli import cli
+
+
+@cli.command('scrape')
+@click.option(
+    '-F',
+    '--group-cif-file',
+    type=GroupParamType(create_if_not_exist=True),
+    required=True,
+    help='Group to store the scraped CIF files in as ``SinglefileData`` nodes.'
+)
+@click.option(
+    '--cache',
+    type=click.Path(dir_okay=False, writable=True),
+    default='amcsd.cache',
+    help='Filepath to the cache.',
+    show_default=True
+)
+@click.option('-M', '--max-number', type=click.INT, required=False, help='Limit number of minerals to scrape.')
+@click.option('-n', '--dry-run', is_flag=True, help='Perform a dry-run and do not store any provenance.')
+def cmd_scrape(group_cif_file, cache, max_number, dry_run):
+    """Scrape the AMCSD to download structures in CIF format.
+
+    The command uses ``beautifulsoup`` to parse the content of ``http://rruff.geo.arizona.eduAMS/all_minerals.php`` for
+    a list of all available minerals. Then for each mineral, its corresponding page is scraped for links that allow to
+    download its structure as a CIF which is then wrapped in a ``SinglefileData`` node and added the ``Group``
+    provided by the ``-F/--group-cif-file`` option.
+
+    The requests are cached which is written to a file on disk provided by the ``--cache`` option.
+    """
+    # pylint: disable=too-many-locals
+    import io
+    import re
+
+    from aiida.common.files import md5_from_filelike
+    from aiida.orm import SinglefileData
+    import bs4
+    import requests
+    import requests_cache
+
+    requests_cache.install_cache(cache)
+
+    base_url = 'http://rruff.geo.arizona.edu'
+    response = requests.get(f'{base_url}/AMS/all_minerals.php')
+    soup_all = bs4.BeautifulSoup(response.text, 'html.parser')
+    minerals = []
+    cif_files = []
+
+    for link in soup_all.find_all('a'):
+
+        href = link.get('href')
+
+        if href.startswith('/AMS/minerals'):
+            minerals.append(href)
+
+    click.echo(click.style('Report: ', fg='blue') + f'found links for {len(minerals)} minerals.')
+
+    with click.progressbar(minerals[:max_number], width=0, bar_template='%(label)-50s [%(bar)s] %(info)s') as links:
+        for link_mineral in links:
+
+            mineral_name = link_mineral.split('/')[-1]
+            links.label = f'Downloading: {mineral_name}'
+
+            try:
+                response_mineral = requests.get(f'{base_url}{link_mineral}')
+            except requests.HTTPError as exc:
+                click.echo(click.style('Error: ', fg='error') + f'retrieving link `{link_mineral}` failed: {exc}')
+                continue
+
+            soup_mineral = bs4.BeautifulSoup(response_mineral.text, 'html.parser')
+
+            for anchor in soup_mineral.find_all('a'):
+
+                if anchor.get_text() != 'Download CIF data':
+                    continue
+
+                link_cif = f"{base_url}{anchor.get('href')}"
+
+                try:
+                    response_cif = requests.get(link_cif)
+                except requests.HTTPError as exc:
+                    click.echo(click.style('Error: ', fg='error') + f'retrieving CIF link `{link_cif}` failed: {exc}')
+                    continue
+
+                matches = re.match(r'^.*?id=([0-9]+)\.cif.*$', link_cif)
+                cif_id = matches.group(1)
+
+                byte_stream = io.BytesIO(response_cif.text.encode('utf-8'))
+                md5 = md5_from_filelike(byte_stream)
+                byte_stream.seek(0)
+
+                cif_file = SinglefileData(file=byte_stream, filename=f'{mineral_name}_{cif_id}.cif')
+                cif_file.set_attribute('source', {'id': cif_id, 'name': mineral_name, 'url': link_cif})
+                cif_file.set_attribute('md5', md5)
+
+                cif_files.append(cif_file)
+                break
+
+            else:
+                click.echo(click.style('Error: ', fg='error') + f'failed to download a CIF for {mineral_name}')
+
+        click.echo(click.style('Success: ', fg='green') + 'scraping completed, adding to group.')
+        if not dry_run:
+            group_cif_file.add_nodes([cif_file.store() for cif_file in cif_files])
+
+    click.echo(click.style('Success: ', fg='green') + f'stored {len(cif_files)} CIF files in `{group_cif_file.label}`.')
